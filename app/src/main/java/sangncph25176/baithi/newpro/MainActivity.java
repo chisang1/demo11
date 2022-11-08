40749ecdb565e78f2e67928a59796036169fd749
Nguyen chi sang <sangncph25176@fpt.edu.vn>
2022-11-08 13:48:39 +0700

    initproject

---
+++ app/src/main/java/sangncph25176/baithi/newpro/MainActivity.java
@@ -0,0 +1,14 @@
+package sangncph25176.baithi.newpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
